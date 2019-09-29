873b73f7bf5615402d4ae3054015ad6048621ea7
Rus Hughes <russell.hughes@gmail.com>
2019-09-29 13:18:49 +0200

    s16 l108

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+
+    protected $fillable = ['name'];
+
+    public function photos () {
+      return $this->morphMany('App\Photo', 'imageable');
+    }
 }
--- app/Staff.php
+++ app/Staff.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
     //
+
+    public function photos () {
+      return $this->morphMany('App\Photo', 'imageable');
+    }
 }
